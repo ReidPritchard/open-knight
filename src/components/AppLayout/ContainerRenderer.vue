a59253ef22af6928577df6e55bd9c3e743c0c838
Reid Pritchard <reidpritchard99@gmail.com>
2024-10-31 14:16:48 -0600

    undo tsconfig changes

--- src/components/AppLayout/ContainerRenderer.vue
+++ src/components/AppLayout/ContainerRenderer.vue
@@ -1,156 +1,171 @@
 <template>
-    <Panel :header="props.layout.title" :toggleable="props.layout.collapsible" :collapsed="props.layout.collapsed"
-        :style="{
-            transformOrigin: 'bottom right',
-            rotate: props.layout.collapsed ? '-90deg' : '0deg',
-            width: props.layout.collapsed ? '40px' : 'auto',
-        }" @update:collapsed="props.layout.collapsed = $event">
-        <div :style="containerStyle">
-            <div v-if="props.layout.display === 'flexible'" :class="['flex-container', orientationClass]">
-                <template v-for="child in props.layout.children" :key="child.id">
-                    <LayoutRenderer :layout="child" />
-                </template>
-            </div>
+  <Panel
+    :header="props.layout.title"
+    :toggleable="props.layout.collapsible"
+    :collapsed="props.layout.collapsed"
+    :style="{
+      transformOrigin: 'bottom right',
+      rotate: props.layout.collapsed ? '-90deg' : '0deg',
+      width: props.layout.collapsed ? '40px' : 'auto',
+    }"
+    @update:collapsed="props.layout.collapsed = $event"
+  >
+    <div :style="containerStyle">
+      <div
+        v-if="props.layout.display === 'flexible'"
+        :class="['flex-container', orientationClass]"
+      >
+        <template v-for="child in props.layout.children" :key="child.id">
+          <LayoutRenderer :layout="child" />
+        </template>
+      </div>
 
-            <div v-else-if="props.layout.display === 'tabs'">
-                <Tabs v-model:activeIndex="activeTabIndex">
-                    <TabPanel v-for="(child) in props.layout.children" :header="child.title || child.id"
-                        :key="child.id">
-                        <LayoutRenderer :layout="child" />
-                    </TabPanel>
-                </Tabs>
-            </div>
+      <div v-else-if="props.layout.display === 'tabs'">
+        <Tabs v-model:activeIndex="activeTabIndex">
+          <TabPanel
+            v-for="child in props.layout.children"
+            :header="child.title || child.id"
+            :key="child.id"
+          >
+            <LayoutRenderer :layout="child" />
+          </TabPanel>
+        </Tabs>
+      </div>
 
-            <div v-else-if="props.layout.display === 'simple'" class="simple-container">
-                <div v-if="props.layout.top" class="top-panel">
-                    <LayoutRenderer :layout="props.layout.top" />
-                </div>
-                <div class="middle-content">
-                    <div v-if="props.layout.left" class="left-panel">
-                        <LayoutRenderer :layout="props.layout.left" />
-                    </div>
-                    <div class="center-content">
-                        <template v-for="child in props.layout.children" :key="child.id">
-                            <LayoutRenderer :layout="child" />
-                        </template>
-                    </div>
-                    <div v-if="props.layout.right" class="right-panel">
-                        <LayoutRenderer :layout="props.layout.right" />
-                    </div>
-                </div>
-                <div v-if="props.layout.bottom" class="bottom-panel">
-                    <LayoutRenderer :layout="props.layout.bottom" />
-                </div>
-            </div>
+      <div
+        v-else-if="props.layout.display === 'simple'"
+        class="simple-container"
+      >
+        <div v-if="props.layout.top" class="top-panel">
+          <LayoutRenderer :layout="props.layout.top" />
         </div>
-    </Panel>
+        <div class="middle-content">
+          <div v-if="props.layout.left" class="left-panel">
+            <LayoutRenderer :layout="props.layout.left" />
+          </div>
+          <div class="center-content">
+            <template v-for="child in props.layout.children" :key="child.id">
+              <LayoutRenderer :layout="child" />
+            </template>
+          </div>
+          <div v-if="props.layout.right" class="right-panel">
+            <LayoutRenderer :layout="props.layout.right" />
+          </div>
+        </div>
+        <div v-if="props.layout.bottom" class="bottom-panel">
+          <LayoutRenderer :layout="props.layout.bottom" />
+        </div>
+      </div>
+    </div>
+  </Panel>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { IWindowContainer, validateFlexibleContainer, validateTabContainer } from '../../shared/types';
-import LayoutRenderer from './LayoutRenderer.vue';
-import Tabs from 'primevue/tabs';
-import TabPanel from 'primevue/tabpanel';
-import Panel from 'primevue/panel';
-import { useGlobalState } from '../../shared/store';
+import { computed, ref } from "vue";
+import {
+  IWindowContainer,
+  validateFlexibleContainer,
+  validateTabContainer,
+} from "../../shared/types";
+import LayoutRenderer from "./LayoutRenderer.vue";
+import Tabs from "primevue/tabs";
+import TabPanel from "primevue/tabpanel";
+import Panel from "primevue/panel";
+import { useGlobalState } from "../../shared/store";
 
 const { updateWindowProperty } = useGlobalState();
 
 const props = defineProps({
-    layout: {
-        type: Object as () => IWindowContainer,
-        required: true,
-    },
+  layout: {
+    type: Object as () => IWindowContainer,
+    required: true,
+  },
 });
 
 const orientationClass = computed(() => {
-    const flexibleContainer = validateFlexibleContainer(props.layout);
-    if (flexibleContainer.success) {
-        return flexibleContainer.data.orientation === 'vertical'
-            ? 'flex-column'
-            : 'flex-row';
-    }
-    return '';
+  const flexibleContainer = validateFlexibleContainer(props.layout);
+  if (flexibleContainer.success) {
+    return flexibleContainer.data.orientation === "vertical"
+      ? "flex-column"
+      : "flex-row";
+  }
+  return "";
 });
 
 const containerStyle = computed(() => {
-    const style: any = {
-        display: 'flex',
-        flexDirection: 'column',
-    };
-    if (props.layout.fixedSize) {
-        style.flexBasis = `${props.layout.fixedSize}px`;
-        style.flexShrink = 0;
-        style.flexGrow = 0;
-    } else {
-        style.flexGrow = props.layout.size || 1;
-    }
-    return style;
+  const style: any = {
+    display: "flex",
+    flexDirection: "column",
+  };
+  if (props.layout.fixedSize) {
+    style.flexBasis = `${props.layout.fixedSize}px`;
+    style.flexShrink = 0;
+    style.flexGrow = 0;
+  } else {
+    style.flexGrow = props.layout.size || 1;
+  }
+  return style;
 });
 
 const activeTabIndex = ref(
-    props.layout.children.findIndex(
-        (child) => {
-            const tabContainer = validateTabContainer(child);
-            return tabContainer.success && tabContainer.data.activeTabId;
-        }
-    )
+  props.layout.children.findIndex((child) => {
+    const tabContainer = validateTabContainer(child);
+    return tabContainer.success && tabContainer.data.activeTabId;
+  }),
 );
 </script>
 
 <style scoped>
 * {
-    --p-panel-content-padding: 0;
-    --p-panel-toggleable-header-padding: 0;
+  --p-panel-content-padding: 0;
+  --p-panel-toggleable-header-padding: 0;
 }
 
-
 .content {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
 }
 
 .flex-container {
-    display: flex;
-    width: 100%;
-    height: 100%;
+  display: flex;
+  width: 100%;
+  height: 100%;
 
-    justify-content: stretch;
+  justify-content: stretch;
 }
 
 .flex-row {
-    flex-direction: row;
+  flex-direction: row;
 }
 
 .flex-column {
-    flex-direction: column;
+  flex-direction: column;
 }
 
 .simple-container {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .top-panel,
 .bottom-panel {
-    flex-shrink: 0;
+  flex-shrink: 0;
 }
 
 .middle-content {
-    display: flex;
-    flex: 1;
+  display: flex;
+  flex: 1;
 }
 
 .left-panel,
 .right-panel {
-    flex-shrink: 0;
+  flex-shrink: 0;
 }
 
 .center-content {
-    flex: 2;
-    display: flex;
-    flex-direction: column;
+  flex: 2;
+  display: flex;
+  flex-direction: column;
 }
 </style>
